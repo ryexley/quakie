9b12d961581716324eae0f3e0c6250c0deb4ef6d
Bob Yexley <bob@yexley.net>
2016-05-11 23:04:11 -0400

    Versioning localization files in `/src` and copying them over via the build command

--- package-scripts.js
+++ package-scripts.js
@@ -1,8 +1,11 @@
+const buildDefaultCommand = "clear && cp -rf ./src/i18n/locales ./server/public && webpack --config ./webpack.config.js --progress";
+const buildWatchCommand = "clear && cp -rf ./src/i18n/locales ./server/public && webpack --config ./webpack.config.js --watch --progress";
+
 module.exports = {
   scripts: {
     build: {
-      default: "clear && webpack --config ./webpack.config.js --progress",
-      watch: "clear && webpack --config ./webpack.config.js --watch --progress"
+      default: buildDefaultCommand,
+      watch: buildWatchCommand
     },
     server: {
       default: "clear && DEBUG=quakie* ./node_modules/.bin/node-dev ./bin/server"
